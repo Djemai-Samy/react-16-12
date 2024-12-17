ca61ab62dccb5412e7423e5a960dca45e7b37a56
Djemai Samy <samy.djemai93@gmail.com>
2024-12-17 10:52:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from './composants/containers/Header/Header';
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App;
+
+// Exercice:
+// Créer un composant Footer dans "/containers";
+// Afficher dans App.jsx
+// Pour les réponses:
+// Créer Un Repository sur Github
+// Fair un push
+// Envoyer le lien au formateur
