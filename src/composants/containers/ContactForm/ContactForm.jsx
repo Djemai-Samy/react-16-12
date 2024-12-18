8ab632dadb614437723c3c1fe24323f41fb6dbee
Djemai Samy <samy.djemai93@gmail.com>
2024-12-18 16:01:45 +0100

    Cours: Formulaires

--- src/composants/containers/ContactForm/ContactForm.jsx
+++ src/composants/containers/ContactForm/ContactForm.jsx
@@ -6,16 +6,36 @@ import Bouton from "../../ui/Bouton/Bouton";
 export default function ContactForm() {
 	// Variable pour stocker les données entrées dans le champs
 	const [fullNameInput, setFullNameInput] = useState('Samy');
+	const [fullNameError, setFullNameError] = useState('');
+
+	const [messageInput, setMessageInput] = useState('');
+	const [messageError, setMessageError] = useState('');
+
 	function gereFullName(event) {
 		// Récuperer l'entrée de l'utilisateur
 		const valeur = event.target.value;
 		// Stocker la valeur de l'input dans la variable d'état
 		setFullNameInput(valeur);
+		setFullNameError('');
+	}
+
+	function gereMessage(e) {
+		setMessageInput(e.target.value);
+		setMessageError('');
 	}
 
 	function submit() {
+		if (fullNameInput === "") {
+			setFullNameError('Nom et prénom obligatoire !');
+			return
+		}
+		if (messageInput === "" || messageInput > 500) {
+			setMessageError('Message Obligatoire ! (Max: 500)')
+			return
+		}
 		// Récuperer les données du formulaire
-		alert('Contact ' + fullNameInput)
+		alert('Contact ' + fullNameInput);
+		setFullNameInput('');
 	}
 
 	return (
@@ -27,6 +47,13 @@ export default function ContactForm() {
 				className="bg-neutral-800 p-2 w-full"
 				type="text"
 				placeholder="Nom et prénom" />
+			<p className="text-red-600 text-sm">{fullNameError}</p>
+
+			<textarea
+				className="bg-neutral-800 p-2 w-full"
+				onChange={gereMessage}
+				placeholder="Entrez un message">{messageInput}</textarea>
+			<p>{messageError}</p>
 			<Bouton gereClique={submit}>Envoyer le message</Bouton>
 		</div>
 	)
