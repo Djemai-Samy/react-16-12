3b4e2ef574ab32614bd16ecf480f0a076f13295d
Djemai Samy <samy.djemai93@gmail.com>
2024-12-18 11:39:04 +0100

    Cours: Evenements

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Bouton from './composants/ui/Bouton/Bouton';
 import "./index.css";
 
 function App() {
-
   const user = {
     prenom: "John",
     nom: "Doe",
@@ -16,16 +15,22 @@ function App() {
       { id: 'react', nom: "React" }]
   }
 
+  function ajoute() {
+    alert('Ajouter')
+  }
+  function supprime() {
+    alert('Supprimer')
+  }
   return (
     <>
       <div>
         <Header />
-        <Bouton couleur={"green"}>
+        <Bouton couleur={"green"} gereClique={ajoute}>
           <img src='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNvcHktcGx1cyI+PGxpbmUgeDE9IjE1IiB4Mj0iMTUiIHkxPSIxMiIgeTI9IjE4Ii8+PGxpbmUgeDE9IjEyIiB4Mj0iMTgiIHkxPSIxNSIgeTI9IjE1Ii8+PHJlY3Qgd2lkdGg9IjE0IiBoZWlnaHQ9IjE0IiB4PSI4IiB5PSI4IiByeD0iMiIgcnk9IjIiLz48cGF0aCBkPSJNNCAxNmMtMS4xIDAtMi0uOS0yLTJWNGMwLTEuMS45LTIgMi0yaDEwYzEuMSAwIDIgLjkgMiAyIi8+PC9zdmc+' />
           <p>Ajouter</p>
         </Bouton>
 
-        <Bouton couleur={'red'}>
+        <Bouton couleur={'red'} gereClique={supprime}>
           <p>Supprimer</p>
         </Bouton>
 
--- src/composants/ui/Bouton/Bouton.jsx
+++ src/composants/ui/Bouton/Bouton.jsx
@@ -2,13 +2,15 @@ import PropTypes from 'prop-types';
 
 Bouton.propTypes = {
   couleur: PropTypes.string,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  gereClique: PropTypes.func.isRequired
 }
 
-export default function Bouton({ couleur, children }) {
+export default function Bouton({ couleur, children, gereClique }) {
 
   return (
     <button
+      onClick={gereClique}
       className="b-none p-2 rounded hover:opacity-75 transition"
       style={{ backgroundColor: couleur ? couleur : "black" }}
     >
